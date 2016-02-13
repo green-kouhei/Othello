3bfe9861810e8314711f529eeda88d55d0274a01
green-kouhei <mintopazu@gmail.com>
2016-02-14 00:48:58 +0900

    ReversにTo_reversを追加

--- src/o_kinou/Set.java
+++ src/o_kinou/Set.java
@@ -3,66 +3,44 @@ package o_kinou;
 import java.util.Scanner;
 
 public class Set {
-	public static int gx;
-	public static int gy;
-	public int[][] set(int[][] s,int collar){
+//	public void re(){
+//	//int Y = 0, X = 0;//To_reverse用変数
+//	}
+
+	public int[][] set(int[][] s, int collar) {
+		int Y = 0, X = 0;//To_reverse用変数
 		Scanner sin = new Scanner(System.in);
-		Change g = new Change();
-		for(;;){
+		for (;;) {
 			System.out.println("置く場所を指定してください");
-			System.out.println("0～7までとa～h");
+			System.out.println("０～７までと０～７");
 			int y = sin.nextInt();
-			if(y < 0 || y > 7){
+			if (y < 0 || y > 7) {
 				System.out.println("範囲外です");
 				continue;
+			} else {
+				Y = y;
 			}
-			String s1 = sin.next();
-			int x = g.change(s1);
-			if(x < 0 || x > 7){
+			int x = sin.nextInt();
+			if (x < 0 || x > 7) {
 				System.out.println("範囲外です");
 				continue;
+			} else {
+				X = x;
 			}
-			if(s[y][x] == 3){
+			if (s[y][x] == 3) {
 				s[y][x] = collar;
 				break;
-			}
-			else{
+			} else {
 				System.out.println("ここには置けません");
 			}
 		}
-		for(int y = 0;y<8;y++){
-			for(int x = 0;x < 8;x++){
-				if(s[y][x] == 3){
+		for (int y = 0; y < 8; y++) {
+			for (int x = 0; x < 8; x++) {
+				if (s[y][x] == 3) {
 					s[y][x] = 0;
 				}
 			}
 		}
 		return s;
 	}
-}
-
-class Change{
-	int change(String v){
-		switch(v){
-		case "a":
-			return 1;
-		case "b":
-			return 2;
-		case "c":
-			return 3;
-		case "d":
-			return 4;
-		case "e":
-			return 5;
-		case "f":
-			return 6;
-		case "g":
-			return 7;
-		case "h":
-			return 8;
-		default:
-			return 0;
-		}
-	}
-
 }
