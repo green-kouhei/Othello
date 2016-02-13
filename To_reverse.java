da54c058f059c95583b811a86fa788c253594aee
green-kouhei <mintopazu@gmail.com>
2016-02-14 00:24:23 +0900

    ひっくり返すメゾット作成（To_reverse）とそのアルゴリズム記述

---
+++ To_reverse.java
@@ -0,0 +1,17 @@
+package o_kinou;
+
+import o_kinou.Set;
+import o_main.Hairetu_test;
+
+public class To_reverse {
+
+	public static int reverse(int[][] z) {
+		//白ターンと仮定
+		//置いた場所セット
+		//Hanteiメゾットちょっと変えたやつ動かす
+		//白黒・・・白になったら中の黒を白に
+		//ターンエンド！
+		return 0;
+	}
+
+}
