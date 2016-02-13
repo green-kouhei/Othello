e74d2f18c0ca13b86889a94f50ef5c0e030a0023
OsatoShouta <o_syouta@sftbank.ne.jp>
2016-02-14 00:22:23 +0900

    作業中

--- src/o_kinou/Set.java
+++ src/o_kinou/Set.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Set {
 	public int[][] set(int[][] s,int collar){
 		Scanner sin = new Scanner(System.in);
+		Change g = new Change();
 		for(;;){
 			System.out.println("置く場所を指定してください");
-			System.out.println("０～７までと０～７");
+			System.out.println("0～7までとa～h");
 			int y = sin.nextInt();
 			if(y < 0 || y > 7){
 				System.out.println("範囲外です");
 				continue;
 			}
-			int x = sin.nextInt();
+			String s1 = sin.next();
+			int x = g.change(s1);
 			if(x < 0 || x > 7){
 				System.out.println("範囲外です");
 				continue;
@@ -35,4 +37,30 @@ public class Set {
 		}
 		return s;
 	}
+}
+
+class Change{
+	int change(String v){
+		switch(v){
+		case "a":
+			return 1;
+		case "b":
+			return 2;
+		case "c":
+			return 3;
+		case "d":
+			return 4;
+		case "e":
+			return 5;
+		case "f":
+			return 6;
+		case "g":
+			return 7;
+		case "h":
+			return 8;
+		default:
+			return 0;
+		}
+	}
+	
 }
