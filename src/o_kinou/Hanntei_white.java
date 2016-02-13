7b70dba9d8dd9d97e8b3ba24e9118b22c82006e9
OsatoShouta <o_syouta@sftbank.ne.jp>
2016-02-13 15:33:41 +0900

    ホワイトリバース【仮】

--- src/o_kinou/Hanntei_white.java
+++ src/o_kinou/Hanntei_white.java
@@ -6,12 +6,23 @@ public class Hanntei_white {
 			for(int y = 0;y < 8;y++){
 				switch (s[x][y]) {
 				case (1):
-					loop1:
 					for(int a = -1;a<=1;a++){
 						for(int b = -1;b<=1;b++){
 							if(s[x+a][y+b] == 2){
-							x = x+a; y = y+b;
+								loop:
 								for(;;){
+									x = x+a; y = y+b;
+									if(s[x+a][y+b] == 2){
+									continue loop;
+									}
+									else if(s[x+a][y+b] == 1){
+										break;
+									}
+									else if(s[x+a][y+b] == 0){
+										s[x+a][y+b] = 3;
+										break;
+									}
+									break;
 								}
 							}
 						}
