ba8574da461614066a942959a1b17b816fd573a5
green-kouhei <mintopazu@gmail.com>
2016-02-14 01:04:41 +0900

    To_reverseのアルゴリズムに追加記述

--- src/o_kinou/Reverse.java
+++ src/o_kinou/Reverse.java
@@ -13,7 +13,7 @@ class To_reverse {
 	public static int reverse(int[][] z) {
 		// 白ターンと仮定
 		// 置いた場所セット
-		// Hanteiメゾットちょっと変えたやつ動かす
+		// Hanteiメゾットちょっと変えたやつ(全部のマス見なくていいため)動かす
 		// 白黒・・・白になったら中の黒を白に
 		// ターンエンド！
 		return 0;
