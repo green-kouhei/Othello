58b04b2255ac17ab6a817defe5a505dd16fac1fe
green-kouhei <mintopazu@gmail.com>
2016-02-14 00:57:19 +0900

    class To_reverse追加

--- src/o_kinou/Reverse.java
+++ src/o_kinou/Reverse.java
@@ -1,8 +1,22 @@
 package o_kinou;
 
 public class Reverse {
-	public int[][] reverse(int[][] s){
-		
+	public int[][] reverse(int[][] s) {
+
 		return s;
 	}
+
+}
+
+class To_reverse {
+
+	public static int reverse(int[][] z) {
+		// 白ターンと仮定
+		// 置いた場所セット
+		// Hanteiメゾットちょっと変えたやつ動かす
+		// 白黒・・・白になったら中の黒を白に
+		// ターンエンド！
+		return 0;
+	}
+
 }
